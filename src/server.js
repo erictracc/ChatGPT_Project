99a3cf6670de03c116b0f3dca0574b373461a382
ItzFlip <nicholas.veronico@gmail.com>
2024-11-11 16:22:51 -0500

    fix: add code for connecting to db

--- src/server.js
+++ src/server.js
@@ -1,5 +1,9 @@
 const config = require('../config');
 const app = require('./app');
+const connectToDatabase = require('./database/database');
+
+// Conenct to the database
+connectToDatabase();
 
 const port = config.web.port;
 app.listen(port, () => {
