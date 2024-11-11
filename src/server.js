f367b1025c75b8c0df195a516d1d87c483ac48bb
ItzFlip <nicholas.veronico@gmail.com>
2024-11-11 16:13:47 -0500

    style: organize web server files

--- src/server.js
+++ src/server.js
@@ -1,24 +1,7 @@
-const express = require('express');
-const app = express();
-const mongoose = require('mongoose');
-const config = require('../config.json');
+const config = require('../config');
+const app = require('./app');
 
-// Connect to the database
-connectToDatabase(config.mongodb.url).catch(error => console.error(error));
-
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
-});
-
-const port = 7548;
+const port = config.web.port;
 app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
+    console.log(`Server running on https://itec4220.itzflip.ca`);
 });
-
-
-async function connectToDatabase(url) {
-    await mongoose.connect(url, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    });
-}
